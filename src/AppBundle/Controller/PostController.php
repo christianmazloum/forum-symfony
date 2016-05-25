ee7a0600a8680d66def674ce8c47708668861a6d
Christian Mazloum <mazloumchristian@gmail.com>
2016-05-25 11:56:08 +0200

    fetching data from articles table

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Article;
 
 class PostController extends Controller
 {
@@ -28,8 +29,20 @@ class PostController extends Controller
           ],
         ];
 
+        $article = $this->getDoctrine()
+        ->getRepository('AppBundle:Article')
+        ->findAll();
+
+        if (!$article) {
+          throw $this->createNotFoundException(
+          'No product found for id '.$id
+          );
+        }
+
+
         // replace this example code with whatever you need
         return $this->render('posts/index.html.twig', [
+          'articles' => $article,
           'variable' => $var,
           'tables' => $tab,
           'produit' => $product
